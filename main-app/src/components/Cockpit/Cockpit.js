8f3dcb6465f5cd1aadbc4c17831d98323c61325c
Carlos <carloshximenes@gmail.com>
2019-09-11 06:44:31 -0300

    Demonstration App

---
+++ main-app/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Cockpit.module.css';
+
+const cockpit = (props) => {
+    const assignedClasses = [];
+    let btnClass = '';
+
+    if (props.showPersons) btnClass = styles.Red;
+    if (props.persons.length <= 2) assignedClasses.push(styles.red);
+    if (props.persons.length <= 1) assignedClasses.push(styles.bold);
+
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <p className={assignedClasses.join(' ')}>This is really working!</p>
+            <button
+                className={btnClass}
+                onClick={props.clicked}>Toggle Persons</button>
+        </div>
+    );
+}
+
+export default cockpit;
